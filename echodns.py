3c4bf6d824f0b3cda60da6beb4e1c6d2ab5be6ca
Francesco Ruvolo <ruvolof@gmail.com>
2024-09-01 02:20:38 +0200

    Adding configuration in config.ini file

--- echodns.py
+++ echodns.py
@@ -1,20 +1,21 @@
+import configparser
 import dnslib
 import socket
 from concurrent.futures import ThreadPoolExecutor
 
 
-def extract_ip_from_domain(domain_name):
-  if domain_name.endswith('.echodns.com.'):
+def extract_ip_from_domain(domain_name, base_domain):
+  if domain_name.endswith(base_domain):
     ip_part = domain_name.split('.')[0]
     ip_address = ip_part.replace('-', '.')
     return ip_address
   return None
 
 
-def handle_dns_query(data, addr, sock):
+def handle_dns_query(data, addr, sock, base_domain):
   request = dnslib.DNSRecord.parse(data)
   domain_name = str(request.q.qname)
-  ip_address = extract_ip_from_domain(domain_name)
+  ip_address = extract_ip_from_domain(domain_name, base_domain)
   if ip_address:
     reply = dnslib.DNSRecord(
         header=request.header,
@@ -27,14 +28,20 @@ def handle_dns_query(data, addr, sock):
 
 
 def main():
+  config = configparser.ConfigParser()
+  config.read('config.ini')
+  bind_address = config['server']['bind_address']
+  bind_port = int(config['server']['bind_port'])
+  base_domain = config['server']['base_domain']
+
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind(('192.168.1.193', 53))
-  print("DNS server is running...")
+  sock.bind((bind_address, bind_port))
+  print(f"EchoDNS server is listening on {bind_address}:{bind_port}")
 
   with ThreadPoolExecutor(max_workers=20) as executor:
     while True:
       data, addr = sock.recvfrom(512)
-      executor.submit(handle_dns_query, data, addr, sock)
+      executor.submit(handle_dns_query, data, addr, sock, base_domain)
 
 
 if __name__ == "__main__":
